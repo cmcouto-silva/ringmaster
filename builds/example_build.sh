336b23c6ba71aada58fe05db46e0dbb3473cf992
Matt Christie <christiemj09@gmail.com>
2017-11-02 16:18:52 -0500

    POC for a database build process with tests injected.

--- builds/example_build.sh
+++ builds/example_build.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 
-# Build an example database. Formal description of the build process.
+# Build process POC. Build a toy example database.
+# Constitutes a formal description of a specific build with tests.
 
-alias run="python scripts/cmd.py"
+# Stop if nonzero error code; this is how program flow is controlled.
+set -e
 
-run wipe_tables
-# Put tests here; pipe them into a Python program that suspends if there are
-# errors or failed tests.
+# Get database credentials from configuration file
+GET="./scripts/get.py .creds"
 
-run init_tables
-# Put tests here
+# Make psql pretty :) Do we need to worry about shell injection?
+RUN="psql -U $($GET user) -d $($GET dbname) -h $($GET host) -c"
+CHECK="psql -t -U $($GET user) -d $($GET dbname) -h $($GET host) -F , --no-align --pset footer -f"
 
-run pop_tables
-# Put tests here
+### BUILD PROCESS STARTS HERE ###
+
+# Could inspect the shorthand first, I suppose, as a guard against injection before it's run
+
+echo "RUN is:"
+echo "$RUN"
+
+echo
+
+echo "CHECK is:"
+echo "$CHECK"
+
+echo
+
+./scripts/pause.py
+
+$RUN "SELECT wipe_tables();"
+$CHECK tests/test_wipe_tables.sql > tests/test.out 2>&1
+./scripts/checkpoint.py  # Flags you can pass: dirty strict quiet
+
+$RUN "SELECT init_tables();"
+$CHECK tests/test_init_tables.sql > tests/test.out 2>&1
+./scripts/checkpoint.py
+
+$RUN "SELECT pop_tables();"
+$CHECK tests/test_pop_tables.sql > tests/test.out 2>&1
+./scripts/checkpoint.py
+
+# Question for Dan: Do you wrap your build process in one big transaction?
+# Would need to modify this if so, commits are made after each high-level build function.
+
